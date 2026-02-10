2a88972b09c348441a29ef85baa6bacfa79e6697
Rana Mohamed <ranabessar002@gmail.com>
2026-02-10 03:22:30 +0200

    fix: remove accidentally pasted logs from outputs.tf

--- terraform/modules/api-gateway/outputs.tf
+++ terraform/modules/api-gateway/outputs.tf
@@ -1,20 +1,3 @@
-2026-02-10T00:39:22.4174325Z ##[section]Starting: Validate AWS Credentials
-2026-02-10T00:39:22.4183810Z ==============================================================================
-2026-02-10T00:39:22.4183960Z Task         : Command line
-2026-02-10T00:39:22.4184046Z Description  : Run a command line script using Bash on Linux and macOS and cmd.exe on Windows
-2026-02-10T00:39:22.4184184Z Version      : 2.268.0
-2026-02-10T00:39:22.4184267Z Author       : Microsoft Corporation
-2026-02-10T00:39:22.4184372Z Help         : https://docs.microsoft.com/azure/devops/pipelines/tasks/utility/command-line
-2026-02-10T00:39:22.4184488Z ==============================================================================
-2026-02-10T00:39:22.7841852Z Generating script.
-2026-02-10T00:39:22.7863610Z ========================== Starting Command Output ===========================
-2026-02-10T00:39:22.7914308Z [command]/usr/bin/bash --noprofile --norc /home/devops/myagent/_work/_temp/bb9c9e6f-1574-4a96-a822-2401750f12fe.sh
-2026-02-10T00:39:22.8050937Z Validating AWS credentials...
-2026-02-10T00:39:24.9925489Z 
-2026-02-10T00:39:24.9931797Z An error occurred (InvalidClientTokenId) when calling the GetCallerIdentity operation: The security token included in the request is invalid.
-2026-02-10T00:39:25.2654913Z 
-2026-02-10T00:39:25.2739552Z ##[error]Bash exited with code '254'.
-2026-02-10T00:39:25.2801260Z ##[section]Finishing: Validate AWS Credentials
 # ============================================================================
 # API Gateway Module Outputs
 # ============================================================================
