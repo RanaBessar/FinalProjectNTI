ce4adf69435e4de6556ecdc8885d1a68c906e8d1
Rana Mohamed <ranabessar002@gmail.com>
2026-02-11 23:08:56 +0200

    edits

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "nti-finalproject-terraform-state"
+    bucket         = "nti-finalproject-terraform-state-rana2"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "nti-finalproject-terraform-lock"
+    dynamodb_table = "nti-finalproject-terraform-lock-rana2"
     encrypt        = true
   }
 }
