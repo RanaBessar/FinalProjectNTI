080b879e76323e5cb70b07a14f4040c14d7924d5
RanaBessar <ranabessar002@gmail.com>
2026-02-12 23:07:49 +0200

    edits

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "nti-final-tfstate-842303506852-us-east-1-2026"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "nti-final-tflock-842303506852-us-east-1-2026"
+    dynamodb_table = "nti-finalproject-terraform-lock"
     encrypt        = true
   }
 }
