68fd359c084558d42e2ecca385a9a614970a89e3
RanaBessar <ranabessar002@gmail.com>
2026-02-13 00:49:46 +0200

    edits

--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "cluster_version" {
 
 variable "node_instance_types" {
   type    = list(string)
-  default = ["t3.medium"]
+  default = ["m7i-flex.large"]
 }
 
 variable "desired_size" {
